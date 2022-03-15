32745f5fef87065e6f994dc0a03cffd8fbbfe31a
Maciej Sikora <maciejsikora2302@gmail.com>
2022-03-15 19:23:41 +0100

    added pdf, updated jupyter notebook

--- test.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import pyplot as plt
-
-
-fig, axs = plt.subplots(2, 2)
-fig.suptitle('Vertically stacked subplots')
-axs[0, 0].plot(range(10), range(10))
-axs[1, 0].plot(range(10), range(10))
-plt.show()
